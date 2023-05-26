a76edb5915b925bb679c69e77933f54b0f32c044
Lakshmi Sankar <plsankar1996@gmail.com>
2023-05-26 11:14:16 +0530

    chore: added the google analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import TemplatesProvider from "@/contexts/TemplatesProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import toast, { Toaster } from "react-hot-toast";
+import Script from "next/script";
+import { Toaster } from "react-hot-toast";
 import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'GA_MEASUREMENT_ID');
+                `}
+            </Script>
             <SWRConfig
                 value={{
                     fetcher: (resource, init) =>
